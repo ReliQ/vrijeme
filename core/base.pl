e3a95b694bf0283d319a84b473fe63b7b27c68d9
reliq <versatile.jamrok@gmail.com>
2014-11-05 14:44:52 -0500

    ...

--- core/base.pl
+++ core/base.pl
@@ -6,29 +6,4 @@ system('Vrijeme - v0.0.1'). 	%System info
 
 
 wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
-wind_speed(12 to 40, 'Average speed').
 

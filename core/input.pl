efc5c5ad85dbe7c73c0351f690059d5373909d9f
reliq <versatile.jamrok@gmail.com>
2014-11-07 18:59:52 -0500

    Progress

--- core/input.pl
+++ core/input.pl
@@ -1,17 +1,17 @@
-%%Vrijeme
-%%input.pl
+%% Vrijeme
+%% input.pl
 
-system('Vrijeme - v0.0.1').     %System info
-
-write('Please Enter the following weather variables:').
-humidity_input(Humidity,Status):-
-                write('1.Humidity:'), read(Humidity), write ('%').
+humidityInput(Humidity, Status):-
+    write('Humidity:'), read(Humidity), write('%').
                 
-seaTemperature_input(Temperature,Status):-
-                write('2.Sea Temperature:'), read(Temperature), write ('^C').
+sstInput(Temperature, Status):-
+    nl, write('Sea Temperature:'), read(Temperature), write('C').
                 
-airPressure_input(Pressure,Status):-
-                write('3.Pressure:'), read(Pressure).
+airPressureInput(Pressure, Status):-
+	nl, write('Pressure:'), read(Pressure).
                 
-windspeed_input(Speed,Status):-
-                               write('4.Wind Speed (m/s):'), read(Speed).
+windspeedInput(Speed, Status):-
+    nl, write('Wind Speed (mph):'), read(Speed).
+    
+getInput:-
+    write('get inputs').
--- core/main.pl
+++ core/main.pl
@@ -2,3 +2,19 @@
 %%	main.pl 
 
 :- [base, input]. 
+
+
+run:-
+    system(X),
+    string_concat('Welcome to ', X, WelcomeMessage),
+    write(WelcomeMessage), nl, write('Select Option:'), nl,
+    write('1. Information Input'), nl, write('2. View History'), nl,
+    write('3. Get Prediction'), nl,
+    write('-| Choice: '), read(Choice), doOption(Choice).
+      
+      
+doOption(Choice):-
+    integer(Choice),
+    string_concat('time to do...', Choice, X), nl,
+    (Choice == 1 -> getInput; write(X)).
+

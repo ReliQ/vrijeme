27091661c4837d78d42e694b749145d1cddbebd7
kiddsilver12 <b.findlay242@gmail.com>
2014-11-08 14:36:10 -0700

    Names of variables were modified to be cohesive throughout code
    Line one is still giving errors

--- core/input.pl
+++ core/input.pl
@@ -1,27 +1,30 @@
-﻿%% Vrijeme
+﻿% Author:
+% Date: 11/8/2014
+
+%% Vrijeme
 %% input.pl
 
+
 humidityInput(Humidity):-
-    write('Humidity (%): '), read(Humidity), 
+    write('Humidity (%): '), read(Humidity),
     Humidity < 1. %- Ensure Humidity is less than 1
-                
+
 sstInput(Temperature):-
     write('Sea Temperature (°c): '), read(Temperature).
-                
+
 airPressureInput(Pressure):-
     write('Pressure (mb): '), read(Pressure).
-                
+
 windSpeedInput(Speed):-
     write('Wind Speed (mph): '), read(Speed).
-    
+
 lapseRateInput(LapseRate):-
     write('Lapse Rate (°c/km): '), read(LapseRate).
-    
-    
-collectReading(Humidity, SeaSurfaceTemperature, AirPressure, WindSpeed, LapseRate):-
+
+collectReading(Humidity, Temperature, Pressure, Speed, LapseRate):-
     write('New Reading...'), nl,
-    humidityInput(Humidity), 
-    sstInput(SeaSurfaceTemperature), 
-    airPressureInput(AirPressure),
-    windSpeedInput(WindSpeed),
+    humidityInput(Humidity),
+    sstInput(Temperature),
+    airPressureInput(Pressure),
+    windSpeedInput(Speed),
     lapseRateInput(LapseRate).

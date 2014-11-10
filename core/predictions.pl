c103bb4704d1b01e1abef1c737c42fe5da98296d
Yondaimeku <Daimeku@gmail.com>
2014-11-10 02:17:50 -0500

    quickfix, fixed condition where status changes are mixed

--- core/predictions.pl
+++ core/predictions.pl
@@ -4,25 +4,16 @@
 :- ensure_loaded([base, file, warnings]).
 
 prediction:-
-            %memory_file(X),
-            %open(X, read, Source),
-            %read(Source, [H1,T1,P1,S1,M1]),          %reading values from file, H=humidity T=sea_temperature P=pressure S=wind_speed M=moist_airtemperature
-            %read(Source, [H2,T2,P2,S2,M2]),
-            %read(Source, [H3,T3,P3,S3,M3]),
-            %close(Source),
-            %getLines([[H1,T1,P1,S1,M1],[H2,T2,P2,S2,M2],[H3,T3,P3,S3,M3]]),
             getLines(Lines),
             revl([D1,D2,D3|T],Lines),
             conditions(D1,D2,D3,Level),
-            %conditions([H1,T1,P1,S1,M1], [H2,T2,P2,S2,M2], [H3,T3,P3,S3,M3], Level),
             write('conditions: '), write(Level), nl,
             wind_cat(D1,WeatherStatus),                  %testing for storms based on the wind speed
             write('weather status: '), write(WeatherStatus),nl,
             warning(WeatherStatus),nl,
             displayPrediction(Level),nl,
-            displayConditions(D1,D2,D3). %stormExists 1= there is a storm 0 = there is no storm
-            %(WeatherStatus =:= -1)->StormExists is 0; StormExists is 1,
-            %write('Storm exists?: '), write(StormExists).
+            displayConditions(D1,D2,D3).
+
              
 conditions([H1,T1,P1,S1,M1], [H2,T2,P2,S2,M2], [H3,T3,P3,S3,M3], Level):-     %evaluates weather trends and determines weather conditions are decline or improving
                           HumidityChange is (H1-H2)+(H2-H3),         % 0 = stable weather(no changes)
@@ -31,8 +22,9 @@ conditions([H1,T1,P1,S1,M1], [H2,T2,P2,S2,M2], [H3,T3,P3,S3,M3], Level):-     %e
                           SpeedChange is (S1-S2)+(S2-S3),
                           MoistTempChange is (M1-M2)+(M2-M3),
                           ( (HumidityChange>0) ; (SpeedChange>0) ; (TempChange>0) ; (MoistTempChange>0) -> Level is 2;true),
-                          ( (SpeedChange<0)->(HumidityChange<0)->(TempChange<0)->(MoistTempChange<0)-> Level is 1; true),%check for good weather
-                          ( (SpeedChange=:=0),(HumidityChange =:=0),(TempChange=:=0),(MoistTempChange=:=0)-> Level is 0; Level is 3).  %check for normal weather ELSE mixed weather conditions
+                          ( (SpeedChange<0),(HumidityChange<0),(TempChange<0),(MoistTempChange<0)-> Level is 1; true),%check for good weather
+                          ( (SpeedChange=:=0),(HumidityChange =:=0),(TempChange=:=0),(MoistTempChange=:=0)-> Level is 0; true),
+                          ( (integer(Level)->true;Level is 3) ).  %check for normal weather ELSE mixed weather conditions
 
 displayConditions([H1,T1,P1,S1,M1], [H2,T2,P2,S2,M2], [H3,T3,P3,S3,M3]):-
                HumidityChange is (H1-H2)+(H2-H3),         % 0 = stable weather(no changes)
@@ -62,7 +54,8 @@ displayConditions([H1,T1,P1,S1,M1], [H2,T2,P2,S2,M2], [H3,T3,P3,S3,M3]):-
                write('\tWind Speed: '),write(S3),
                write('\tAtmospheric Convection: '),write(M3),
                write('\tPressure: '),write(P3).
-               
+
+%displays prediction of whether the weather is improving, stable, deteriorating or mixed
 displayPrediction(Level):-
      Level==0
              ->write('\tBased on the analysis, stable weather is predicted'),nl;
@@ -73,18 +66,21 @@ displayPrediction(Level):-
      Level==3
              ->write('\tMixed weather conditions (see below)'),nl;true.
 
+%reads all the lines of a file into a list of lists
 getLines(L):-
    memory_file(X),
-  setup_call_cleanup(
+  setup_call_cleanup(    %setup & read from the file, then close
     open(X, read, In),
     readData(In, L),
     close(In)
   ).
 
+%reads a line of data and appends it to the list until end_of_file is reached
 readData(In, L):-
   read_term(In, H, []),
        ( H == end_of_file -> L = [] ; L = [H|T],
        readData(In,T)).
-  
+
+%reverses a list
 revl([], []).
 revl(R, [H|T]) :- revl(RT, T), append(RT, [H], R).
